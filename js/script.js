8e8d7bfaf3ef465f6d4d6294cdde57d19c958853
amr <amrjok00@gmail.com>
2023-07-29 13:42:31 +0200

    signin page

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.getElementById("sidebarCollapse").onclick = function () {
+  document.getElementById("sidebar").classList.toggle("active");
+};
+
+document.querySelector(".nav_btn").onclick = function () {
+  document.getElementById("navbarSupportedContent").classList.toggle("hide");
+};
