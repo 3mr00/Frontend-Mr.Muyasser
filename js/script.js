52cf1025021bdc67ea90161e4f90e9111a39ddfc
amr <amrjok00@gmail.com>
2023-08-03 20:35:51 +0200

    lesson & single chapter pages

--- js/script.js
+++ js/script.js
@@ -1,7 +1,27 @@
-document.getElementById("sidebarCollapse").onclick = function () {
-  document.getElementById("sidebar").classList.toggle("active");
-};
+const sidebarCollapse = document.getElementById("sidebarCollapse");
 
-document.querySelector(".nav_btn").onclick = function () {
-  document.getElementById("navbarSupportedContent").classList.toggle("hide");
-};
+if (sidebarCollapse) {
+  sidebarCollapse.addEventListener("click", function () {
+    document.getElementById("sidebar").classList.toggle("active");
+  });
+}
+
+// JavaScript to control the play button functionality
+const playButton = document.getElementById("custom-play-button");
+const videoPlayer = document.getElementById("video-player");
+
+playButton.addEventListener("click", function () {
+  videoPlayer.play();
+  playButton.style.display = "none";
+  videoPlayer.style.filter = "none";
+});
+
+videoPlayer.addEventListener("play", function () {
+  playButton.style.display = "none";
+  videoPlayer.style.filter = "none";
+});
+
+videoPlayer.addEventListener("pause", function () {
+  playButton.style.display = "block";
+  videoPlayer.style.filter = "brightness(0.5)";
+});
