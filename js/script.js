3a6c3283be703c07e2cc85d0b92f4eb922529f30
amr <amrjok00@gmail.com>
2023-08-05 13:46:42 +0200

    exam page

--- js/script.js
+++ js/script.js
@@ -10,18 +10,30 @@ if (sidebarCollapse) {
 const playButton = document.getElementById("custom-play-button");
 const videoPlayer = document.getElementById("video-player");
 
-playButton.addEventListener("click", function () {
-  videoPlayer.play();
-  playButton.style.display = "none";
-  videoPlayer.style.filter = "none";
-});
+if (playButton) {
+  playButton.addEventListener("click", function () {
+    videoPlayer.play();
+    playButton.style.display = "none";
+    videoPlayer.style.filter = "none";
+  });
+  videoPlayer.addEventListener("play", function () {
+    playButton.style.display = "none";
+    videoPlayer.style.filter = "none";
+  });
 
-videoPlayer.addEventListener("play", function () {
-  playButton.style.display = "none";
-  videoPlayer.style.filter = "none";
-});
+  videoPlayer.addEventListener("pause", function () {
+    playButton.style.display = "block";
+    videoPlayer.style.filter = "brightness(0.5)";
+  });
+}
 
-videoPlayer.addEventListener("pause", function () {
-  playButton.style.display = "block";
-  videoPlayer.style.filter = "brightness(0.5)";
+//max width exam
+
+const max_w = document.querySelector(".max_w");
+
+max_w.addEventListener("click", function () {
+  document.getElementById("div_exam").classList.toggle("col-xl-9");
+  document.getElementById("div_content").classList.toggle("col-xl-3");
+  document.getElementById("close_svg").classList.toggle("open_svg");
+  document.getElementById("open_svg").classList.toggle("close_svg");
 });
